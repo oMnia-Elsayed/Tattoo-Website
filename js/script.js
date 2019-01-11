abd7d38245765425c198dd5f67150304c14c77bd
unknown <omniamorsi2020@gmail.com>
2019-01-11 23:14:16 +0200

    version 1 not responsive

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// import { setInterval } from "timers";
+
+$('.carousel').carousel({
+    interval: 2000
+  });
+
+
+var image = document.querySelectorAll(".cards .card-body img");
+
+// image.forEach(element => {
+//   setTimeout(() => {
+//     element.classList.toggle("border-active");
+//     // console.log(element.nextElementSibling);
+//   }, 2000);
+// });
